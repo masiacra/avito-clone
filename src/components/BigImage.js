d3cbbab02f99b4dcfab35e7831b97c0cadc183fa
masiacra <masiacra@gmail.com>
2019-08-08 12:16:19 +0400

    Changed index.css, BigImage.js
      Improved component display

--- src/components/BigImage.js
+++ src/components/BigImage.js
@@ -5,18 +5,19 @@ function BigImage(props) {
 	const className= props.isHidden ? 'big_image hidden' : 'big_image';
 	return (
 		<div className={className}>
-		  <div className="big_image___image">
-			  <img
-				  src={props.src}
-				  alt={props.title}
-			  />
-			  <div 
-			    className="big_image__closing_btn"
-			    onClick={props.clickHandlerForClosing}
-			  >
-			    x
-			  </div>
+		  <div className="big_image__wrapper">
+		  <img
+			src={props.src}
+			alt={props.title}
+			className="big_image__img"
+		   />
+		   <div 
+			  className="big_image__closing_btn"
+			  onClick={props.clickHandlerForClosing}
+			>
+			  x
 			</div>
+		  </div>
 		</div>		
 	);
 }
